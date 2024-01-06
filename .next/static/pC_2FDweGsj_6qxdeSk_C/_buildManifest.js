be82c9f4f595f65af2367f9f0c2f292edf2ef1ca
SamaadTurner <samaad.turner@gmail.com>
2024-01-06 12:22:35 -0800

    build commnad

--- .next/static/pC_2FDweGsj_6qxdeSk_C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2cca2479-b91a2136bca3490f.js","static/chunks/630-cfe2b85c6353cd14.js","static/chunks/pages/index-e6b81ead1ca93f85.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
